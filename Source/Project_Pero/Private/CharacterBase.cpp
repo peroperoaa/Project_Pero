f63bfe893f290d9625344f80b191b98c4be9b1e6
peroperoaa <a3046229569@163.com>
2025-08-12 00:41:33 +0800

    添加胡桃模型及其相关动画

--- Source/Project_Pero/Private/CharacterBase.cpp
+++ Source/Project_Pero/Private/CharacterBase.cpp
@@ -8,7 +8,7 @@ ACharacterBase::ACharacterBase()
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-	//AbilitySystemComponent = CreateDefaultSubobject<UAbilitySystemComponent>(TEXT("AbilitySystemComponent"));
+	AbilitySystemComponent = CreateDefaultSubobject<UAbilitySystemComponent>(TEXT("AbilitySystemComponent"));
 }
 
 // Called when the game starts or when spawned
